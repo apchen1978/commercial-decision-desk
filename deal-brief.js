0e8dc737112c9a98dacb3e3398a3a14aa7cc376f
AP <paulchen1978@gmail.com>
2026-08-24 19:34:53 +0800

    fix: clean deal brief executive copy

--- deal-brief.js
+++ deal-brief.js
@@ -187,6 +187,7 @@ function exportReason(reason, language) {
     .replace(/\s+([,.;])/g, "$1")
     .replace(/\(\s*(?:,\s*)+\)/g, "")
     .replace(/\(\s*\)/g, "")
+    .replace(/\s{2,}/g, " ")
     .trim();
 }
 
